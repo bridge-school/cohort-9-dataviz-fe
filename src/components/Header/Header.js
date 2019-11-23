fbcf7812085c92823825dd51734441756eecece1
IrinaGM <irina.golionko@gmail.com>
2019-11-22 23:29:39 -0500

    styled Header component using styled-components. changed logos and site title.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,27 @@
 import React from "react";
+import logo from "../../bridgeLogo.svg";
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: white;
+  background-color: rgb(78, 87, 202);
+`;
+
+const Logo = styled.img`
+  max-width: 100%;
+  max-height: 75%;
+  padding: 6px 2vh;
+`;
+
 
 export const Header = () => {
-  return <div className="header">This is Header</div>;
+  return (
+    <HeaderStyle className="header">
+      <Logo className="logo" src={logo} alt="Logo" />
+      Cohort Application Data
+    </HeaderStyle>
+  );
 };
