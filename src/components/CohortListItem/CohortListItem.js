7f6a509e9877b327038160f92b8feafafeb7a8c2
Marta <marta@Martas-MacBook-Air.local>
2019-11-25 21:12:25 -0500

    Add style

--- src/components/CohortListItem/CohortListItem.js
+++ src/components/CohortListItem/CohortListItem.js
@@ -5,7 +5,7 @@ import { ListItem } from "./CohortListItemStyle";
 const CohortListItem = ({ cohortID, key }) => {
   return (
     <ListItem key={key}>
-      <Link to={`/cohorts/${cohortID}`}>
+      <Link to={`/cohorts/${cohortID}`} style={{color: '#000', textDecoration: 'none' }}>
         {cohortID.replace("-", " ")}
       </Link>
     </ListItem>
