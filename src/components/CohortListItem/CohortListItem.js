9f2790aed9cb90b01cbb502607d3d0728f1fd29c
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-11-27 10:52:50 -0500

    Ordering cohorts beginning with the most recent

--- src/components/CohortListItem/CohortListItem.js
+++ src/components/CohortListItem/CohortListItem.js
@@ -1,12 +1,15 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-import { ListItem } from "./CohortListItemStyle";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { ListItem } from './CohortListItemStyle';
 
 const CohortListItem = ({ cohortID, key }) => {
   return (
     <ListItem key={key}>
-      <Link to={`/cohorts/${cohortID}`} style={{color: '#000', textDecoration: 'none' }}>
-        {cohortID.replace("-", " ")}
+      <Link
+        to={`/cohorts/${cohortID}`}
+        style={{ color: '#000', textDecoration: 'none' }}
+      >
+        Cohort {cohortID}
       </Link>
     </ListItem>
   );
