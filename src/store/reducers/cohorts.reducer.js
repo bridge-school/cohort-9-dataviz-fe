3c59048a502483f92811770417f485bbc0224f3f
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-11-27 21:33:29 -0500

    simplified name of initial state

--- src/store/reducers/cohorts.reducer.js
+++ src/store/reducers/cohorts.reducer.js
@@ -1,10 +1,10 @@
 import { COHORTS } from '../actions/actions.type';
 
-export const INITIAL_COHORTS_STATE = {
+export const INITIAL_STATE = {
   cohortApplicants: {}
 };
 
-export const cohortsReducer = (state = INITIAL_COHORTS_STATE, action = {}) => {
+export const cohortsReducer = (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
     case COHORTS.SET_DATA:
       return { ...state, cohortApplicants: action.payload };
