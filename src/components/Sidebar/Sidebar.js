a461f53772582ebd39aef80d8f58044b6c6d46d0
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-12-05 16:29:46 -0500

    Removes unnecessary Button component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
-import { Button } from '../Button/Button';
 import { SidebarStyle } from './SidebarStyle';
 
 export const Sidebar = () => {
@@ -8,18 +7,10 @@ export const Sidebar = () => {
 
   return (
     <SidebarStyle>
-      <Link to={`${url}/gender-identity`}>
-        <Button text="gender identity" />
-      </Link>
-      <Link to={`${url}/minority-group`}>
-        <Button text="minority groups" />
-      </Link>
-      <Link to={`${url}/previous-bootcamp`}>
-        <Button text="dev bootcamp" />
-      </Link>
-      <Link to={`${url}/employment-status`}>
-        <Button text="employment status" />
-      </Link>
+      <Link to={`${url}/gender-identity`}>gender identity</Link>
+      <Link to={`${url}/minority-group`}>minority groups</Link>
+      <Link to={`${url}/previous-bootcamp`}>dev bootcamp</Link>
+      <Link to={`${url}/employment-status`}>employment status</Link>
     </SidebarStyle>
   );
 };
