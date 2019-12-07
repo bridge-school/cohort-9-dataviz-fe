0b9d649445ab3db0d692bfd7979e20d4293ce878
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-12-07 14:31:06 -0500

    Uses theme variable instead of hard-coded colour value

--- src/views/GraphSectionStyle.js
+++ src/views/GraphSectionStyle.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { theme } from '../GlobalStyle';
 
 export const GraphSectionStyle = styled.div`
   grid-column-start: 2;
   display: flex;
   flex-direction: column;
   align-self: auto;
-  background-color: #ffffff;
+  background-color: ${theme.color.white};
   height: 100%;
   padding-right: 40px;
 `;
