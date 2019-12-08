ca708db15647c699ee44bcae52f9ea3ec2f0f6ce
IrinaGM <irina.golionko@gmail.com>
2019-12-08 12:48:38 -0500

    Added title for line graph in HomePage

--- src/components/Graphs/LineGraph.js
+++ src/components/Graphs/LineGraph.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { GraphTitle } from './GraphTitle';
 
 import {
   LineChart,
@@ -15,29 +16,32 @@ export default class LineGraph extends PureComponent {
   render() {
     const reversed = [...this.props.cohorts].reverse();
     return (
-      <ResponsiveContainer width="99%" height={400}>
-        <LineChart
-          data={reversed}
-          margin={{
-            top: 5,
-            right: 5,
-            left: 5,
-            bottom: 5
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="id" />
-          <YAxis type="number" domain={[40, 50]} />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="applicants"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <>
+        <GraphTitle text={'Number of Applicants Per Cohort'} />
+        <ResponsiveContainer width="99%" height={400}>
+          <LineChart
+            data={reversed}
+            margin={{
+              top: 5,
+              right: 5,
+              left: 5,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="id" />
+            <YAxis type="number" domain={[40, 50]} />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="applicants"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </>
     );
   }
 }
