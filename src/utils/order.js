294b81923a70d1905349e104af7614d90d5b4474
martapa <martabioinf@gmail.com>
2019-12-08 13:52:25 -0500

    Fix empty bar

--- src/utils/order.js
+++ src/utils/order.js
@@ -6,7 +6,7 @@ export const orderBars = (dataArr, lastBar) => {
   );
 
   const sortedFiltered =
-    filtered[0].name == 'Other' ? filtered : [filtered[1], filtered[0]];
+    filtered[0].name == 'Other' ? filtered : filtered.reverse();
 
   const filtered2 = descData.filter(
     object => object.name !== 'Other' && object.name !== lastBar
